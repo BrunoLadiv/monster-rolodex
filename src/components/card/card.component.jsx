cc9488f5543e00c037a25b71eca3039b6ad2eb5f
Bruno Vidal <brunokasprik@gmail.com>
2022-12-03 00:01:21 -0300

    change img res

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -5,7 +5,7 @@ class Card extends Component {
     render() {
         const { id, name, email } = this.props.monster;
         return (<div className="card-container">
-            <img src={`https://robohash.org/${id}?set=set2&size=180x180`} alt={`monster ${name}`} />
+            <img src={`https://robohash.org/${id}?set=set2&size=280x280`} alt={`monster ${name}`} />
         
             <h2 h2 > { name }</h2 >
             <p>{email}</p> </div>)
